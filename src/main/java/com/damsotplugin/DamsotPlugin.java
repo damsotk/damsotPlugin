fa89bf0ec8fb12537231342cd4e046c35dbed8c7
damsot <denispiyack@gmail.com>
2024-10-24 21:13:06 +0200

    mobSpawn update

--- src/main/java/com/damsotplugin/DamsotPlugin.java
+++ src/main/java/com/damsotplugin/DamsotPlugin.java
@@ -9,39 +9,29 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class DamsotPlugin extends JavaPlugin implements CommandExecutor, Listener {
 
     @Override
     public void onEnable() {
-        
         getCommand("spawnRazlom").setExecutor(this);
-    
-        
-        Bukkit.getPluginManager().registerEvents(new magicCerebrum(this), this);
-    
+        Bukkit.getPluginManager().registerEvents(this, this);
         getLogger().info("RiftPlugin включен!");
     }
+
     @Override
     public void onDisable() {
         getLogger().info("RiftPlugin вимкнено!");
     }
 
-   
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("spawnRazlom")) {
-            World world = Bukkit.getWorld("Arnhold"); 
+            World world = Bukkit.getWorld("Arnhold");
             if (world != null) {
-                
                 Location[] locations = {
                     new Location(world, 300, 80, -200),
                     new Location(world, 590, 94, -367),
@@ -49,33 +39,27 @@ public class DamsotPlugin extends JavaPlugin implements CommandExecutor, Listene
                     new Location(world, 241, 79, 395)
                 };
 
-                
                 Random random = new Random();
                 Location selectedLocation = locations[random.nextInt(locations.length)];
 
-                
                 createRift(selectedLocation);
 
-                
                 String message = "§6[§r§fКовенант§r§6]§r §6Кошмары вырвались наружу! Разлом замечен на§r §6"
                         + selectedLocation.getBlockX() + " "
                         + selectedLocation.getBlockY() + " "
                         + selectedLocation.getBlockZ();
                 Bukkit.broadcastMessage(message);
 
-                
-                spawnCustomMob(selectedLocation);
+                startMobWaves(selectedLocation);
             }
             return true;
         }
         return false;
     }
 
-    
     public void createRift(Location location) {
         World world = location.getWorld();
         if (world != null) {
-            
             world.getBlockAt(location.clone().add(0, 0, 0)).setType(Material.RED_WOOL);
             world.getBlockAt(location.clone().add(1, 0, 0)).setType(Material.RED_WOOL);
             world.getBlockAt(location.clone().add(-1, 0, 0)).setType(Material.RED_WOOL);
@@ -84,19 +68,36 @@ public class DamsotPlugin extends JavaPlugin implements CommandExecutor, Listene
         }
     }
 
-    
-    public void spawnCustomMob(Location location) {
-        String command = "mm mobs spawn Forester"; 
-        
-        String commandWithLocation = command + " " + "1 " + location.getWorld().getName() + "," 
+    public void spawnCustomMob(Location location, int amount) {
+        String command = "mm mobs spawn Forester " + amount;
+        String commandWithLocation = command + " " + "1 " + location.getWorld().getName() + ","
                 + location.getBlockX() + ","
-                + (location.getBlockY() - 1) + "," 
+                + (location.getBlockY() - 1) + ","
                 + location.getBlockZ();
 
-        
         Bukkit.getLogger().info("command to try: " + commandWithLocation);
-
-        
         Bukkit.dispatchCommand(Bukkit.getConsoleSender(), commandWithLocation);
+    }
+
+    public void startMobWaves(Location riftLocation) {
+        spawnCustomMob(riftLocation, 2);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                spawnCustomMob(riftLocation, 10);
+            }
+        }.runTaskLater(this, 5 * 60 * 20); 
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                String stabilizationMessage = "§6[§r§fКовенант§r§6]§r §6Разлом на координатах: "
+                        + riftLocation.getBlockX() + " "
+                        + riftLocation.getBlockY() + " "
+                        + riftLocation.getBlockZ() + " стабилизировался";
+                Bukkit.broadcastMessage(stabilizationMessage);
+            }
+        }.runTaskLater(this, 10 * 60 * 20); 
     }
 }
