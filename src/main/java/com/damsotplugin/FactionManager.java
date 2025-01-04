4653d12e5afe7d97b5f3d3471d4e42a7e181898f
damsot <denispiyack@gmail.com>
2025-01-04 07:33:40 +0100

    fix bugs in FactionManager, adding one button for back to main menu, restyle points icons

--- src/main/java/com/damsotplugin/FactionManager.java
+++ src/main/java/com/damsotplugin/FactionManager.java
@@ -8,16 +8,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -28,6 +31,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.yaml.snakeyaml.Yaml;
 
+import io.th0rgal.oraxen.api.OraxenItems;
 import me.clip.placeholderapi.PlaceholderAPI;
 
 public class FactionManager implements CommandExecutor, Listener {
@@ -55,17 +59,17 @@ public class FactionManager implements CommandExecutor, Listener {
 
         if (capturePoints.isEmpty()) {
             capturePoints.put("Заброшенная шахта", "Никто");
-            capturePoints.put("Точка 2", "Никто");
-            capturePoints.put("Точка 3", "Никто");
+            capturePoints.put("Неизвестная деревня", "Никто");
+            capturePoints.put("Росток", "Никто");
         }
 
         capturePointLocations.put("Заброшенная шахта", new Location(Bukkit.getWorld("Arnhold"), -1618, 23, 1499));
-        capturePointLocations.put("Точка 2", new Location(Bukkit.getWorld("Arnhold"), 1173, 170, -555));
-        capturePointLocations.put("Точка 3", new Location(Bukkit.getWorld("Arnhold"), 1173, 170, -553));
+        capturePointLocations.put("Неизвестная деревня", new Location(Bukkit.getWorld("Arnhold"), -510, 18, 2325));
+        capturePointLocations.put("Росток", new Location(Bukkit.getWorld("Arnhold"), -1257, 128, 1601));
 
         pointResources.put("Заброшенная шахта", Material.IRON_INGOT);
-        pointResources.put("Точка 2", Material.BOOK);
-        pointResources.put("Точка 3", Material.DIAMOND);
+        pointResources.put("Неизвестная деревня", Material.BOOK);
+        pointResources.put("Росток", Material.DIAMOND);
 
         for (String point : capturePoints.keySet()) {
             pointTreasuries.put(point, new HashMap<>());
@@ -102,7 +106,7 @@ public class FactionManager implements CommandExecutor, Listener {
 
         Inventory menu = Bukkit.createInventory(null, 54, title);
 
-        ItemStack topPlayersItem = new ItemStack(Material.NETHER_STAR);
+        ItemStack topPlayersItem = OraxenItems.getItemById("icon_razlom").build();
         ItemMeta topPlayersMeta = topPlayersItem.getItemMeta();
         if (topPlayersMeta != null) {
             topPlayersMeta.setDisplayName("§fСписок закрывающих разломы");
@@ -165,7 +169,7 @@ public class FactionManager implements CommandExecutor, Listener {
 
         menu.setItem(37, topPlayersItem);
 
-        ItemStack membersItem = new ItemStack(Material.EMERALD);
+        ItemStack membersItem = OraxenItems.getItemById("icon_people").build();
         ItemMeta membersMeta = membersItem.getItemMeta();
         if (membersMeta != null) {
             List<String> lore = getFactionMembers(faction);
@@ -190,136 +194,144 @@ public class FactionManager implements CommandExecutor, Listener {
             String pointName = entry.getKey();
             String ownerFaction = entry.getValue();
 
-            ItemStack pointItem = new ItemStack(pointResources.get(pointName));
+            
+            String iconId = "icon_avanpost" + (i - 48 + 1); 
+            ItemStack pointItem = OraxenItems.getItemById(iconId).build(); 
+
             ItemMeta pointMeta = pointItem.getItemMeta();
             if (pointMeta != null) {
-                pointMeta.setDisplayName(pointName);
-                pointMeta.setLore(List.of(
-                        "§7Владелец: " + ownerFaction,
-                        "§8Ресурс: " + (pointName.equals("Точка 2") ? "Книги" : pointResources.get(pointName).name()),
-                        "§7Количество: " + pointTreasuries.get(pointName).getOrDefault(faction, 0)
-                ));
+                pointMeta.setDisplayName("§f" + pointName);
+
+                List<String> lore = new ArrayList<>();
+
+                lore.add("§7Владелец: " + ownerFaction);
+
+                
+                switch (pointName) {
+                    case "Заброшенная шахта":
+                        lore.add("§8Когда-то здесь копали ихор");
+                        break;
+                    case "Неизвестная деревня":
+                        lore.add("§8Когда-то здесь жило много мудрецов");
+                        break;
+                    case "Росток":
+                        lore.add("§8Кажется дерево наполнено магией");
+                        break;
+                    default:
+                        lore.add("§8Ресурс: Неизвестно");
+                        break;
+                }
+
+                
+                lore.add("§7Кол-во данного ресурса " + pointTreasuries.get(pointName).getOrDefault(faction, 0));
+
+                pointMeta.setLore(lore);
                 pointItem.setItemMeta(pointMeta);
             }
+
             menu.setItem(i, pointItem);
             i++;
         }
 
+        ItemStack menuCommandItem = OraxenItems.getItemById("icon_back").build();
+
+        ItemMeta menuCommandMeta = menuCommandItem.getItemMeta();
+        if (menuCommandMeta != null) {
+            menuCommandMeta.setDisplayName("§7Вернуться обратно в меню");
+            menuCommandItem.setItemMeta(menuCommandMeta);
+        }
+
+        
+        menu.setItem(8, menuCommandItem);
+
         player.openInventory(menu);
     }
 
-
     private final Map<UUID, String> playerOnPoint = new HashMap<>();
     private final Map<String, Long> playerTimeOnPoint = new HashMap<>();
-    private final Map<UUID, Long> lastNotificationTime = new HashMap<>(); 
-    private final long CAPTURE_TIME = 20 * 60 * 1000;
-    
+    private final Map<UUID, Long> lastNotificationTime = new HashMap<>();
+    private final long CAPTURE_TIME = 10 * 60 * 1000;
+
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
         UUID playerId = player.getUniqueId();
         Location playerLocation = player.getLocation();
         String faction = playerFactions.get(playerId);
-    
+
         if (faction == null) {
             return;
         }
-    
+
         for (Map.Entry<String, Location> entry : capturePointLocations.entrySet()) {
             String pointName = entry.getKey();
             Location pointLocation = entry.getValue();
-    
+            String ownerFaction = capturePoints.get(pointName); 
+
+            
             if (playerLocation.getWorld().equals(pointLocation.getWorld())
-                    && playerLocation.distance(pointLocation) < 1.0) {
+                    && playerLocation.distance(pointLocation) < 9.0) {
+
                 
+                if (faction.equals(ownerFaction)) {
+                    return;
+                }
+
                 if (!playerOnPoint.containsKey(playerId) || !playerOnPoint.get(playerId).equals(pointName)) {
-                    
                     playerOnPoint.put(playerId, pointName);
                     playerTimeOnPoint.put(playerId.toString(), System.currentTimeMillis());
-                    lastNotificationTime.put(playerId, 0L); 
+                    lastNotificationTime.put(playerId, 0L);
                     Bukkit.broadcastMessage("§7[§8Троица§7] §7" + "Кто-то пытается захватить точку " + pointName + "!");
                 }
             } else if (playerOnPoint.containsKey(playerId) && playerOnPoint.get(playerId).equals(pointName)) {
-                
                 playerOnPoint.remove(playerId);
                 playerTimeOnPoint.remove(playerId.toString());
-                lastNotificationTime.remove(playerId); 
+                lastNotificationTime.remove(playerId);
                 player.sendMessage("§7[§8Троица§7] Вы покинули точку " + pointName + ".");
             }
         }
     }
-    
+
     public void startCaptureCheckTask() {
         Bukkit.getLogger().info("Запущена задача проверки захвата точек.");
         Bukkit.getScheduler().runTaskTimer(plugin, () -> {
             long currentTime = System.currentTimeMillis();
-    
+
             for (Map.Entry<UUID, String> entry : playerOnPoint.entrySet()) {
                 UUID playerId = entry.getKey();
                 String pointName = entry.getValue();
                 Player player = Bukkit.getPlayer(playerId);
-    
+
                 if (player != null && player.isOnline() && playerTimeOnPoint.containsKey(playerId.toString())) {
                     long timeSpent = currentTime - playerTimeOnPoint.get(playerId.toString());
                     long timeRemaining = CAPTURE_TIME - timeSpent;
-    
+
                     if (timeRemaining <= 0) {
                         // Игрок успешно захватил точку
                         String faction = playerFactions.get(playerId);
-    
+
                         if (faction != null && !capturePoints.get(pointName).equals(faction)) {
                             capturePoints.put(pointName, faction);
                             Bukkit.broadcastMessage("§7[§8Троица§7] §7Аванпост " + pointName + " §7захвачена фракцией " + faction + "!");
                             plugin.getLogger().info("Точка " + pointName + " захвачена фракцией " + faction);
                             saveFactionData();
                         }
-    
-                        
+
                         playerOnPoint.remove(playerId);
                         playerTimeOnPoint.remove(playerId.toString());
                         lastNotificationTime.remove(playerId);
                     } else {
-                        
                         long lastNotify = lastNotificationTime.getOrDefault(playerId, 0L);
                         if (currentTime - lastNotify >= 5000) {
                             int minutes = (int) (timeRemaining / 60000);
                             int seconds = (int) ((timeRemaining % 60000) / 1000);
                             player.sendMessage("§7[§8Троица§7] §7Осталось времени до захвата: " + String.format("%02d:%02d", minutes, seconds));
-                            lastNotificationTime.put(playerId, currentTime); 
+                            lastNotificationTime.put(playerId, currentTime);
                         }
                     }
                 }
             }
-        }, 0L, 20L); 
-    }
-
-    private ItemStack generateRandomBook() {
-        double random = Math.random();
-        String title = "Обычная книга";
-        String lore = "Обычное содержание книги.";
-
-        if (random < 0.7) {
-
-            title = "Обычная книга";
-            lore = "Обычное содержание книги.";
-        } else if (random < 0.9) {
-
-            title = "Эпическая книга";
-            lore = "Редкое содержание книги.";
-        } else {
-
-            title = "Легендарная книга";
-            lore = "Легендарное содержание книги.";
-        }
-
-        ItemStack book = new ItemStack(Material.BOOK);
-        ItemMeta meta = book.getItemMeta();
-        if (meta != null) {
-            meta.setDisplayName(title);
-            meta.setLore(List.of(lore));
-            book.setItemMeta(meta);
-        }
-        return book;
+        }, 0L, 20L);
     }
 
     private void startResourceProductionTask() {
@@ -332,7 +344,7 @@ public class FactionManager implements CommandExecutor, Listener {
                     saveFactionData();
                 }
             });
-        }, 0L, 40L);
+        }, 0L, 24000L);
     }
 
     @EventHandler
@@ -350,28 +362,45 @@ public class FactionManager implements CommandExecutor, Listener {
                     return;
                 }
 
-                if (clickedItem.getType() != Material.BOOK && pointResources.containsValue(clickedItem.getType())) {
-                    String pointName = pointResources.entrySet().stream()
-                            .filter(entry -> entry.getValue().equals(clickedItem.getType()))
-                            .map(Map.Entry::getKey)
-                            .findFirst()
-                            .orElse(null);
+                String pointName = getPointNameFromItem(clickedItem);
 
-                    if (pointName != null) {
-                        int resourceAmount = pointTreasuries.get(pointName).getOrDefault(faction, 0);
-                        if (resourceAmount > 0) {
-                            pointTreasuries.get(pointName).put(faction, 0);
-                            player.getInventory().addItem(new ItemStack(pointResources.get(pointName), resourceAmount));
-                            player.sendMessage("Вы забрали ресурсы из сокровищницы " + pointName + ".");
-                        } else {
-                            player.sendMessage("Сокровищница точки пуста.");
-                        }
+                
+                if (clickedItem.hasItemMeta() && clickedItem.getItemMeta().hasDisplayName()) {
+                    String itemName = clickedItem.getItemMeta().getDisplayName();
+                    if (itemName.equals("§7Вернуться обратно в меню")) {
+                        
+                        player.chat("/menu");
+                        return;
                     }
-                } else if (clickedItem.getType() == Material.BOOK && pointResources.containsValue(Material.BOOK)) {
-                    String pointName = "Точка 2";
-                    int resourceAmount = pointTreasuries.get(pointName).getOrDefault(faction, 0);
+                }
+
+                if (pointName != null && !removeFormatting(pointName).equals("Неизвестная деревня") && pointResources.containsKey(pointName)) {
+                    String formattedPointName = removeFormatting(pointName);  
+                    int resourceAmount = pointTreasuries.get(formattedPointName).getOrDefault(faction, 0);
                     if (resourceAmount > 0) {
-                        pointTreasuries.get(pointName).put(faction, 0);
+                        pointTreasuries.get(formattedPointName).put(faction, 0);
+                        player.getInventory().addItem(new ItemStack(pointResources.get(pointName), resourceAmount));
+                        player.sendMessage("Вы забрали ресурсы из сокровищницы " + pointName + ".");
+                    } else {
+                        player.sendMessage("Сокровищница точки пуста.");
+                    }
+                } else if (pointName != null && removeFormatting(pointName).equals("Росток")) {
+                    String formattedPointName = removeFormatting(pointName);  
+                    int resourceAmount = pointTreasuries.get(formattedPointName).getOrDefault(faction, 0);
+                    if (resourceAmount > 0) {
+                        pointTreasuries.get(formattedPointName).put(faction, 0);
+                        for (int i = 0; i < resourceAmount; i++) {
+                            player.getInventory().addItem(generateRandomEnchantedBook());  
+                        }
+                        player.sendMessage("Вы забрали зачарованные книги из сокровищницы " + pointName + ".");
+                    } else {
+                        player.sendMessage("Сокровищница точки пуста.");
+                    }
+                } else if (pointName != null && removeFormatting(pointName).equals("Неизвестная деревня")) {
+                    String formattedPointName = removeFormatting(pointName);  
+                    int resourceAmount = pointTreasuries.get(formattedPointName).getOrDefault(faction, 0);
+                    if (resourceAmount > 0) {
+                        pointTreasuries.get(formattedPointName).put(faction, 0);
                         for (int i = 0; i < resourceAmount; i++) {
                             player.getInventory().addItem(generateRandomBook());
                         }
@@ -382,6 +411,74 @@ public class FactionManager implements CommandExecutor, Listener {
                 }
             }
         }
+    }
+
+    private String removeFormatting(String input) {
+        return input.replaceAll("§[0-9a-fk-or]", "");  
+    }
+
+    private ItemStack generateRandomBook() {
+        double random = Math.random();
+        String title = "&7Обычныя свиток";
+        String lore2 = " ";
+        String lore3 = "&7&lМагический предмет";
+        String lore4 = "&7Для обмена у торговца";
+
+        if (random < 0.7) {
+            title = "&7Обычныя свиток";
+        } else if (random < 0.9) {
+            title = "&5Редкий свиток";
+        } else {
+            title = "&6Легендарный свиток";
+        }
+
+        
+        title = ChatColor.translateAlternateColorCodes('&', title);
+        lore2 = ChatColor.translateAlternateColorCodes('&', lore2);
+        lore3 = ChatColor.translateAlternateColorCodes('&', lore3);
+        lore4 = ChatColor.translateAlternateColorCodes('&', lore4);
+
+       
+        ItemStack book = new ItemStack(Material.MOJANG_BANNER_PATTERN);
+        ItemMeta meta = book.getItemMeta();
+
+        if (meta != null) {
+            meta.setDisplayName(title);
+            List<String> loreList = new ArrayList<>();
+            loreList.add(lore2);
+            loreList.add(lore3);
+            loreList.add(lore4);
+            meta.setLore(loreList);
+            book.setItemMeta(meta);
+        }
+
+        return book;
+    }
+
+    private ItemStack generateRandomEnchantedBook() {
+        
+        Enchantment[] enchantments = Enchantment.values();
+        Random random = new Random();
+        Enchantment randomEnchantment = enchantments[random.nextInt(enchantments.length)];
+        int level = random.nextInt(randomEnchantment.getMaxLevel()) + 1;
+
+        
+        ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
+        ItemMeta meta = book.getItemMeta();
+
+        if (meta != null) {
+            meta.addEnchant(randomEnchantment, level, true);  
+            book.setItemMeta(meta);
+        }
+
+        return book;
+    }
+
+    private String getPointNameFromItem(ItemStack item) {
+        if (item != null && item.hasItemMeta() && item.getItemMeta().hasDisplayName()) {
+            return item.getItemMeta().getDisplayName();
+        }
+        return null;
     }
 
     private boolean isPlayerAllowedToTakeIron(Player player) {
@@ -434,7 +531,7 @@ public class FactionManager implements CommandExecutor, Listener {
     private void saveDefaultFactionData(File file) {
         try (FileWriter writer = new FileWriter(file)) {
             writer.write("playerFactions: {}\n");
-            writer.write("capturePoints: {Заброшенная шахта: 'Никто', Точка 2: 'Никто', Точка 3: 'Никто'}\n");
+            writer.write("capturePoints: {Заброшенная шахта: 'Никто', Неизвестная деревня: 'Никто', Росток: 'Никто'}\n");
             writer.write("pointTreasuries: {}\n");
         } catch (IOException e) {
             plugin.getLogger().warning("Не удалось сохранить данные по умолчанию");
